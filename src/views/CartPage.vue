fb45bde1c2f13554a6d40a4e64f7b6dbdbcf1cb8
Javier Carreño <carrenojavier015@gmail.com>
2021-02-27 15:17:58 -0800

    Add all my files

---
+++ src/views/CartPage.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>This is the shoppoing cart</h1>
+</template>
+
+<script>
+export default {
+    name: 'CartPage'
+};
+</script>
+
+<style>
+h1 {
+    color:green;
+}
+</style>
---
+++ src/views/ProductsPage.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>This is the Products detail page</h1>
+</template>
+
+<script>
+export default {
+    name: 'ProductsDetailPage'
+};
+</script>
+
+<style>
+h1 {
+    color:blue;
+}
+</style>
